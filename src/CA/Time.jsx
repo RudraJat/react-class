ad0cff146f8f0c8bc4d74abcdccaa2e3db0afc6f
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-11-24 23:51:32 +0530

    up

--- src/CA/Time.jsx
+++ src/CA/Time.jsx
@@ -2,16 +2,28 @@ import {useState, useEffect} from "react";
 
 function Time(){
     const [time, setTime] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
 
     useEffect(()=>{
-        const id = setInterval(()=>{
+        let id; 
+        if(isRunning){
+            id=setInterval(()=>{
             setTime((t)=>t+1)
         },1000);
+    }
 
         return ()=> clearInterval(id);
-    },[]);
+    },[isRunning]);
 
-    return <h1>{time}</h1>;
+    return(
+        <>
+         <h1>{time}</h1>
+
+         <button className="border-2 m-2 " onClick={()=>setIsRunning(true)}>START</button>
+         <button className="border-2 m-2 " onClick={()=>setIsRunning(false)}>STOP</button>
+         <button className="border-2 m-2 " onClick={()=>setTime(0)}>RESET</button>
+         </>
+    )
 }
 
 export default Time;
